9ec3d0162c69f4629e55b95beeefd908ed40f29b
zesha5816 <zesha5816@wrdsb.ca>
2020-10-07 13:32:05 +0000

    Finished the example question

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that will ask the user how much money they wish to invest, what their 	interest rate is (as a full percentage), and how many years they want to invest
+ * @author Ahmad Zeshan
  */
 public class Main {
 
@@ -9,7 +10,27 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create the Scanner
+		Scanner input = new Scanner(System.in);
+		//prompt user for starting balance
+		  System.out.println("Please enter a starting balance");
+			double startingBalance = input.nextDouble();
+			//prompt user for invest rate
+			System.out.println("Please enter your interest rate");
+			double interestRate  = input.nextInt();
+			//prompt user for investment years
+			System.out.println("Please enter the number of years you will invest for");
+			int investInt = input.nextInt();
+			//initialize count 
+			int count = 0;
+			//turn invest rate in 0.01 value
+			interestRate= interestRate * 0.01;
+			//loops the equation the amount of years for investment
+		 for (int i = 0; i < investInt; i++){
+				startingBalance = startingBalance + (interestRate*startingBalance);
+		 }
+		 //outputs the balance to user
+		System.out.println("Your final balance would be $" + startingBalance);
+			
   }
 }
